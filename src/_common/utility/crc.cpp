233b116c364125d472168581548ee2e4c511ce84
andry81 <andry@inbox.ru>
2017-11-22 18:20:29 +0000

    * crc fix according to https://ru.wikipedia.org/wiki/Циклический_избыточный_код
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/bittools/trunk@7 bdae19c0-f436-41d6-a0fe-cf6dffa3ce0f

--- src/_common/utility/crc.cpp
+++ src/_common/utility/crc.cpp
@@ -7,8 +7,10 @@
 namespace
 {
     template <typename T>
-    FORCE_INLINE T _t_crc(const T * table, T crc, const void * buf, size_t size, T crc_init, T xor_in, T xor_out, bool input_reflected, bool result_reflected)
+    FORCE_INLINE T _t_crc(size_t width, const T * table, T crc, const void * buf, size_t size, T crc_init, T xor_in, T xor_out, bool input_reflected, bool result_reflected)
     {
+        ASSERT_GE(sizeof(crc) * CHAR_BIT, width);
+
         const uint8_t * p = (const uint8_t *)buf;
 
         if (!crc && crc_init) crc = crc_init;
@@ -16,12 +18,12 @@ namespace
         crc ^= xor_in;
 
         while (size--) {
-            const uint8_t buf_byte = input_reflected ? utility::reverse(*p++) : *p++;
+            const uint8_t buf_byte = input_reflected ? *p++ : utility::reverse(*p++); // LSB if true
             crc = table[(crc ^ buf_byte) & 0xFF] ^ (crc >> 8);
         }
 
-        if (result_reflected) {
-            crc = utility::reverse(crc);
+        if (!result_reflected) { // already reflected if true
+            crc = (utility::reverse(crc) >> (sizeof(crc) * CHAR_BIT - width));
         }
 
         return crc ^ xor_out;
@@ -41,31 +43,31 @@ namespace utility
         switch (width) {
         case 16:
             switch (polynomial) {
-            case 0x1021: return _t_crc<uint16_t>(g_crc16_1021, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0x8005: return _t_crc<uint16_t>(g_crc16_8005, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0xC867: return _t_crc<uint16_t>(g_crc16_C867, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0x0589: return _t_crc<uint16_t>(g_crc16_0589, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0x3D65: return _t_crc<uint16_t>(g_crc16_3D65, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0x8BB7: return _t_crc<uint16_t>(g_crc16_8BB7, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0xA097: return _t_crc<uint16_t>(g_crc16_A097, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x1021: return _t_crc<uint16_t>(width, g_crc16_1021, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x8005: return _t_crc<uint16_t>(width, g_crc16_8005, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0xC867: return _t_crc<uint16_t>(width, g_crc16_C867, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x0589: return _t_crc<uint16_t>(width, g_crc16_0589, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x3D65: return _t_crc<uint16_t>(width, g_crc16_3D65, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x8BB7: return _t_crc<uint16_t>(width, g_crc16_8BB7, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0xA097: return _t_crc<uint16_t>(width, g_crc16_A097, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
             }
             break;
 
         case 24:
             switch (polynomial) {
-            case 0x864CFB: return _t_crc<uint32_t>(g_crc24_864CFB, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0x5D6DCB: return _t_crc<uint32_t>(g_crc24_5D6DCB, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x864CFB: return _t_crc<uint32_t>(width, g_crc24_864CFB, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x5D6DCB: return _t_crc<uint32_t>(width, g_crc24_5D6DCB, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
             }
             break;
 
         case 32:
             switch (polynomial) {
-            case 0x04C11DB7: return _t_crc<uint32_t>(g_crc32_04C11DB7, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0x1EDC6F41: return _t_crc<uint32_t>(g_crc32_1EDC6F41, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0xA833982B: return _t_crc<uint32_t>(g_crc32_A833982B, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0x814141AB: return _t_crc<uint32_t>(g_crc32_814141AB, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0x741B8CD7: return _t_crc<uint32_t>(g_crc32_741B8CD7, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
-            case 0x000000AF: return _t_crc<uint32_t>(g_crc32_000000AF, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x04C11DB7: return _t_crc<uint32_t>(width, g_crc32_04C11DB7, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x1EDC6F41: return _t_crc<uint32_t>(width, g_crc32_1EDC6F41, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0xA833982B: return _t_crc<uint32_t>(width, g_crc32_A833982B, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x814141AB: return _t_crc<uint32_t>(width, g_crc32_814141AB, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x741B8CD7: return _t_crc<uint32_t>(width, g_crc32_741B8CD7, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
+            case 0x000000AF: return _t_crc<uint32_t>(width, g_crc32_000000AF, crc, buf, size, crc_init, xor_in, xor_out, input_reflected, result_reflected);
             }
             break;
         }
